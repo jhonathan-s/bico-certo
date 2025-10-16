bc59e549b1753abb77a36ef1d15356b50e7e2cc7
Jhonathan <jhonathandasilva2002@gmail.com>
2025-10-16 14:20:20 -0300

    Modal após cadastro

--- script/registro/acessibilidade.js
+++ script/registro/acessibilidade.js
@@ -3,19 +3,24 @@ const abaixarZoom  = document.getElementById('abaixarZoom');
 const aumentarZoomDM = document.getElementById('aumentarZoomDM');
 const abaixarZoomDM = document.getElementById('abaixarZoomDM');
 const containerDireito = document.getElementById('container-direito');
+const modalBox = document.querySelector('.modal-box');
 
 aumentarZoom.addEventListener('click', () => {
-  containerDireito.style.zoom = '125%'; // Zoom in by 125%
+	containerDireito.style.zoom = '125%'; // Zoom in by 125%
+	modalBox.style.zoom = '125%';
 });
   
 abaixarZoom.addEventListener('click', () => {
-  containerDireito.style.zoom = '100%'; // Zoom back to 100% (normal size)
+	containerDireito.style.zoom = '100%'; // Zoom back to 100% (normal size)
+	modalBox.style.zoom = '100%';
 });
 
 aumentarZoomDM.addEventListener('click', () => {
-  containerDireito.style.zoom = '125%'; // Zoom in by 125%
+	containerDireito.style.zoom = '125%'; // Zoom in by 125%
+	modalBox.style.zoom = '125%';
 });
 
 abaixarZoomDM.addEventListener('click', () => {
-  containerDireito.style.zoom = '100%'; // Zoom back to 100% (normal size)
+	containerDireito.style.zoom = '100%'; // Zoom back to 100% (normal size)
+	modalBox.style.zoom = '100%';
 });
